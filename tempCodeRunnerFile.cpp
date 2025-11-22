0cce91f1c501eb7d5cf430e856c0d53d1a92536d
Jeetjha-07 <jeetjha9023@gmail.com>
2025-11-22 21:35:05 +0530

    all commited

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int maxProduct = INT_MIN;
+    // 4 , 5 ,3 ,5 ,3
+    for(int i=2; i<=n; i++){
+       int k = 0;
+       int j = i;
+       while(j<=n){
+        int f = k;
+        int product = 1;
+        while(k<j){
+            product = product*arr[k];
+            if(maxProduct<product){
+                maxProduct = product;
+            }
+            k++;
+        }
+        k= f+1;
+        j++;
+       }
+    }
+    cout<<maxProduct;
+}
